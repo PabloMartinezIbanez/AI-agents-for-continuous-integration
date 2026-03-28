28110b4fa759adf5527b8518742e76963394f983
PabloMartinezIbanez <pablo.martinezibanez@edu.turkuamk.fi>
2026-03-28 11:52:25 +0200

    Fix SonarQube issues and failing tests

---
+++ tests/javascript/test_prueba.js
@@ -0,0 +1,27 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import { suma, resta } from '../../src/calculator/prueba.js';
+
+test('suma dos numeros positivos', () => {
+    assert.equal(suma(2, 3), 5);
+});
+
+test('suma con numeros negativos', () => {
+    assert.equal(suma(-2, -3), -5);
+});
+
+test('resta dos numeros positivos', () => {
+    assert.equal(resta(8, 3), 5);
+});
+
+test('resta con resultado negativo', () => {
+    assert.equal(resta(3, 8), -5);
+});
+
+test('Test resta fallo', () => {
+    assert.equal(resta(-5, -2), -3);
+});
+
+test('Test suma fallo', () => {
+    assert.equal(suma(-5, -2), -7);
+});
